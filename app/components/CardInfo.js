5a259fb5d66c497a3e73c05788e19c1b9c30dcbe
damnnou <skdamn@outlook.com>
2023-12-06 21:04:13 +0300

    style rules block

--- app/components/CardInfo.js
+++ app/components/CardInfo.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-export default function CardInfo() {
+export default function CardInfo({ styles, title, number }) {
   return (
-    <div className="flex items-center border px-4 h-24">
+    <div className={styles + " flex items-center border px-4 h-30 rounded-md"}>
       <Image
         className="text-black font-bold"
         alt="owlLogo"
@@ -10,9 +10,9 @@ export default function CardInfo() {
         width={60}
         height={0}
       />
-      <div className="">
-        <h3>{"title"}</h3>
-        <p className="font-bold text-3xl">{"number"}</p>
+      <div className="ml-4">
+        <h3>{title}</h3>
+        <p className="font-bold text-3xl">{number}</p>
       </div>
     </div>
   );
--- app/components/LotteryEntrance.js
+++ app/components/LotteryEntrance.js
@@ -23,9 +23,38 @@ export default function LotteryEntrance({ account, raffle, signer }) {
   return (
     <div className="flex px-12 py-12 justify-center h-full">
       {raffle ? (
-        <div className="grid grid-cols-4 gap-12 w-full h-full">
-          <div className="col-span-2"></div>
-          <CardInfo />
+        <div className="grid grid-cols-4 grid-rows-5 gap-12 w-full h-full">
+          <div className="col-span-2 row-span-4 bg-black rounded-md"></div>
+          <div className="relative row-span-4 bg-neutral-100 rounded-md">
+            <div className="bg-green-100 rounded-t-md flex justify-center items-center py-4 border-b">
+              <h3 className="text-2xl">Rules</h3>
+            </div>
+            <div className="flex flex-col my-8 px-4 items-center justify-start h-full">
+              <h3 className="text-2xl font-bold">Specify the smaller number</h3>
+              <p className="mt-8">
+                You must determine which number is smaller and press the button
+                with the correct answer
+              </p>
+              <p className="mt-8">
+                As you pass, you will be shown arithmetic expressions. You need
+                to compare them and indicate which number is smaller
+              </p>
+              <Image
+                className="absolute bottom-8"
+                alt="coinsInvest"
+                src="/coinsInvest.svg"
+                width={200}
+                height={0}
+              />
+            </div>
+          </div>
+          <div className="row-span-2 bg-black rounded-md"></div>
+          <CardInfo styles="" title="My place" number="7 W" />
+          <CardInfo styles="" title="My winnings" number="1240" />
+          <CardInfo styles="" title="Prize fund" number="1 234 770" />
+          <CardInfo styles="" title="End of the game" number="01:54:00" />
+          <CardInfo styles="" title="Online" number="175 678" />
+          <CardInfo styles="" title="Best result" number="175 678" />
         </div>
       ) : (
         <div className="flex flex-col mt-24 items-center">
