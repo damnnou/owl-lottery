19e69b7a116544486178eedf6bbe5ef8ce4fa180
damnnou <skdamn@outlook.com>
2023-12-06 22:34:02 +0300

    add balance

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,26 @@
 "use client";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import Image from "next/image";
+import { ethers } from "ethers";
 
 export default function Header({ account, isLoading, toggleWeb3 }) {
+  const [balance, setBalance] = useState(null);
+
+  const setWallet = async () => {
+    if (account) {
+      setBalance(
+        (
+          await new ethers.BrowserProvider(window.ethereum).getBalance(account)
+        ).toString()
+      );
+    }
+  };
+
+  useEffect(() => {
+    setWallet();
+  }, [account]);
+
   return (
     <div className="flex items-center justify-between px-12 py-4 border">
       <div className="flex items-center mr-auto gap-10">
@@ -17,7 +35,12 @@ export default function Header({ account, isLoading, toggleWeb3 }) {
         {account && (
           <>
             <p>My games:</p>
-            <p>My wallet:</p>
+            <p>
+              My wallet:{" "}
+              <span className="font-bold">
+                {balance && ethers.formatUnits(balance).slice(0, 6) + " ETH"}
+              </span>
+            </p>
           </>
         )}
       </div>
